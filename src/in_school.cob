9deff1339d811f891c3f762def76eab1b4566c11
nolbac <nbachtle@gmail.com>
2025-09-09 22:31:06 +0000

    Fixed weird looping issue Updated interface Added debug statements for login procedure

--- src/in_school.cob
+++ src/in_school.cob
@@ -17,6 +17,8 @@
 
        WORKING-STORAGE SECTION.
        01 WS-FILE-STATUS PIC XX.
+           88 FS-SUCCESS        VALUE "00".
+           88 FS-FILE-NOT-FOUND VALUE "35".
        01 WS-USERNAME    PIC X(20).
        01 WS-PASSWORD    PIC X(12).
        01 WS-PASS-LENGTH PIC 9.
@@ -27,47 +29,70 @@
        01 USER-COUNT PIC 9 VALUE 0.
 
        PROCEDURE DIVISION.
+           PERFORM MAIN-PROCEDURE
+           STOP RUN.
        MAIN-PROCEDURE.
-           DISPLAY "-- InSchool Menu --"
-           DISPLAY "1. Log in"
-           DISPLAY "2. Create Account"
+           DISPLAY "-*-----------------------*-"
+           DISPLAY "-*- InSchool Navigation -*-"
+           DISPLAY "-*-----------------------*-"
+           DISPLAY " "
+           DISPLAY "[1] Log in"
+           DISPLAY "[2] Create Account"
+           DISPLAY " "
+           DISPLAY "-*- Input your selection:"
 
+           *> Get User Input
            ACCEPT WS-CHOICE
 
            IF WS-CHOICE = 1
                PERFORM LOGIN-PROCEDURE
-               DISPLAY "TESTTEST"
            ELSE IF WS-CHOICE = 2
                PERFORM CREATE-ACCOUNT-PROCEDURE
            ELSE
-               DISPLAY "[!] Invalid Choice Selected."
+               DISPLAY "Error: Invalid Choice Selected."
                DISPLAY "[!] Terminating Program."
+               STOP RUN
            END-IF
-           STOP RUN.
+           EXIT.
        LOGIN-PROCEDURE.
            *> Open the "user.txt" file
            OPEN INPUT USER-FILE
 
            *> Handle failing to open the file
-           IF WS-FILE-STATUS NOT = "00"
+           IF NOT FS-SUCCESS
                DISPLAY "Failed to open user file. Status: " WS-FILE-STATUS
                DISPLAY "You cannot login at this time."
                DISPLAY "Either no accounts have been created, or the user file is misplaced."
-               STOP RUN
+               CLOSE USER-FILE
+               EXIT
+           ELSE
+               DISPLAY "[DEBUG] File Status: " WS-FILE-STATUS
            END-IF
 
            *> Accept user input
+           DISPLAY " "
+           DISPLAY "-*- Login into your account"
+           DISPLAY " "
            DISPLAY "Username: "
            ACCEPT WS-USERNAME
            DISPLAY "Password: "
            ACCEPT WS-PASSWORD
 
-           *> Attempt Login
+           *> *** Attempt Login
+
+           *> Reset Variables in-case they have been set in a previous loop
+           MOVE 'N' TO END-FLAG
+           MOVE 'N' TO FOUND-FLAG
+
            PERFORM UNTIL END-FLAG = 'Y' OR FOUND-FLAG = 'Y'
+               *> Current line is set to USER-RECORD, which gives us access to the line's `USER-NAME` and `USER-PASSWORD`
                READ USER-FILE INTO USER-RECORD
                    AT END MOVE 'Y' TO END-FLAG
                END-READ
 
+               DISPLAY "[DEBUG] Reading File: "
+               DISPLAY " - Username:" USER-NAME
+               DISPLAY " - Password:" USER-PASSWORD
                IF WS-USERNAME = USER-NAME
                    IF WS-PASSWORD = USER-PASSWORD
                        DISPLAY "Login Successful"
@@ -79,12 +104,13 @@
            END-PERFORM
 
            IF FOUND-FLAG = 'N'
-               DISPLAY "Username not found"
+               DISPLAY "Login credentials are invalid."
            END-IF
+           CLOSE USER-FILE
            EXIT.
 
        CREATE-ACCOUNT-PROCEDURE.
-           OPEN INPUT USER-FILE
+           OPEN I-O USER-FILE
            PERFORM UNTIL END-FLAG = 'Y'
                READ USER-FILE INTO USER-RECORD
                    AT END MOVE 'Y' TO END-FLAG
@@ -110,4 +136,6 @@
                WRITE USER-RECORD
                CLOSE USER-FILE
            END-IF
+
+           CLOSE USER-FILE
            EXIT.
